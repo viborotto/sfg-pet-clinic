65e514ffe846c9907660ec20534b8626361637dd
viborotto <vittoriaborotto@gmail.com>
2020-05-26 21:14:56 -0300

    Added Pets and Contact info. closes #31

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Owner extends Person {
@@ -10,7 +11,7 @@ public class Owner extends Person {
 
     private String telephone;
 
-    private Set<Pet> pets;
+    private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
         return address;
