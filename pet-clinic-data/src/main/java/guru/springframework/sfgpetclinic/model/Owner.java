f702e26cb691084d38ba722f922a56ccd222fb8a
viborotto <vittoriaborotto@gmail.com>
2020-06-01 21:43:16 -0300

    Owner WITH JPA. closes #34

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,16 +1,27 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "address")
     private String address;
 
+    @Column(name = "city")
     private String city;
 
+    @Column(name = "telephone")
     private String telephone;
 
+    //cascade: se o dono for deletado,pet tbm sera
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
