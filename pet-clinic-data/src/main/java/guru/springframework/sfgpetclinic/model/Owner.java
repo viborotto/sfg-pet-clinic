c78eea30910714fbc65b6d3aaf527c80fbe793c1
viborotto <vittoriaborotto@gmail.com>
2020-07-21 22:20:32 -0300

    Create or Update pet implement

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -23,6 +23,9 @@ public class Owner extends Person {
         this.telephone = telephone;
         this.city = city;
         this.pets = pets;
+        if(pets != null) {
+            this.pets = pets;
+        }
     }
 
     @Column(name = "address")
@@ -37,4 +40,35 @@ public class Owner extends Person {
     //cascade: se o dono for deletado,pet tbm sera
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
+
+
+    /**
+     * Return the Pet with the given name, or null if none found for this Owner.
+     *
+     * @param name to test
+     * @return true if pet name is already in use
+     */
+    public Pet getPet(String name) {
+        return getPet(name, false);
+    }
+
+    /**
+     * Return the Pet with the given name, or null if none found for this Owner.
+     *
+     * @param name to test
+     * @return true if pet name is already in use
+     */
+    public Pet getPet(String name, boolean ignoreNew) {
+        name = name.toLowerCase();
+        for (Pet pet : pets) {
+            if (!ignoreNew || !pet.isNew()) {
+                String compName = pet.getName();
+                compName = compName.toLowerCase();
+                if (compName.equals(name)) {
+                    return pet;
+                }
+            }
+        }
+        return null;
+    }
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -15,13 +15,17 @@ import javax.persistence.Table;
 @Table(name = "types")
 public class PetType extends BaseEntity{
 
+    @Builder
+    public PetType(Long id, String name){
+        super(id);
+        this.name = name;
+    }
+
     @Column(name = "name")
     private String name;
 
     @Override
     public String toString() {
-        return "PetType{" +
-                "name='" + name + '\'' +
-                '}';
+        return  name;
     }
 }
